32bf7dae739d5262a7e260b3e2300d99fe71d803
shrishaaa <110020207+shrishaaa@users.noreply.github.com>
2025-11-21 19:08:16 +0545

    modified

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
- const HomePage({super.key});
+  const HomePage({super.key});
 
- 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('simple todo app'),
-        backgroundColor: const Color.fromARGB(255, 77, 44, 208),
-        foregroundColor: const Color.fromARGB(255, 140, 68, 68),
-      );
+        title: const Text('Simple Todo App'),
+        backgroundColor: const Color.fromARGB(255, 204, 133, 215),
+        foregroundColor: const Color.fromARGB(255, 245, 242, 242),
+      ),
+      body: const Center(child: Text('Hello Flutter!')),
+    );
   }
 }
